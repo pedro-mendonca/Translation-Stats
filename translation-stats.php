cd7ad8f0fe7a51e7026a1feb7e1a34c8975585d9
pedro-mendonca <ped.gaspar@gmail.com>
2018-11-06 12:39:02 +0000

    Update plugin header

--- translation-stats.php
+++ translation-stats.php
@@ -1,17 +1,17 @@
 <?php
-/*
-Plugin Name:       Translation Stats
-Plugin URI:        https://github.com/pedro-mendonca/translation-stats
-GitHub Plugin URI: https://github.com/pedro-mendonca/translation-stats
-Description:       Show WordPress.org translation stats in your installed plugins list.
-Version:           0.6.4
-Author:            Pedro Mendonça
-Author URI:        https://pedromendonca.pt
-License:           GPL2
-License URI:       https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:       translation-stats
-Domain Path:       /languages
-*/
+/**
+ * Plugin Name:       Translation Stats
+ * Plugin URI:        https://github.com/pedro-mendonca/translation-stats
+ * GitHub Plugin URI: https://github.com/pedro-mendonca/translation-stats
+ * Description:       Show WordPress.org translation stats in your installed plugins list.
+ * Version:           0.6.4
+ * Author:            Pedro Mendonça
+ * Author URI:        https://pedromendonca.pt
+ * License:           GPL2
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       translation-stats
+ * Domain Path:       /languages
+ */
 
 
 class Plugin_Translation_Stats {
