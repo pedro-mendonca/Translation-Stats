6fc427d6e02d02fa0a4196a9e6abe43500bc0b9a
pedro-mendonca <ped.gaspar@gmail.com>
2018-11-14 16:11:17 +0000

    Escape

--- includes/class-tstats-main.php
+++ includes/class-tstats-main.php
@@ -268,7 +268,7 @@ if ( ! class_exists( 'TStats_Main' ) ) {
 			<div class="translation-stats-title">
 				<?php
 				$url         = 'https://translate.wordpress.org/locale/' . $locale->slug . '/' . $variant . '/wp-plugins/' . $project_slug;
-				$locale_link = '<a href="' . $url . '" _target="blank">' . $locale->native_name . '</a>';
+				$locale_link = '<a href="' . esc_url( $url ) . '" _target="blank">' . $locale->native_name . '</a>';
 				/* translators: %s Language native name. */
 				echo sprintf( wp_kses_post( __( 'Translation for %s', 'translation-stats' ) ), $locale_link );
 				?>
