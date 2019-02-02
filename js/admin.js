f65cd851b022de6959e3983996ffcf237f22d07d
pedro-mendonca <ped.gaspar@gmail.com>
2019-02-02 21:49:17 +0000

    Add Eslint

--- js/admin.js
+++ js/admin.js
@@ -4,24 +4,25 @@ jQuery( document ).ready( function( $ ) {
 	 * Settings tabs navigation.
 	 */
 	var tstatsHash = window.location.hash;
-	if ( tstatsHash !== "" ) {
-		$( ".nav-tab-wrapper" ).children().removeClass( "nav-tab-active" );
-		$( ".nav-tab-wrapper a[href=\"" + tstatsHash + "\"]" ).addClass( "nav-tab-active" );
+	if ( '' !== tstatsHash ) {
+		$( '.nav-tab-wrapper' ).children().removeClass( 'nav-tab-active' );
+		$( '.nav-tab-wrapper a[href="' + tstatsHash + '"]' ).addClass( 'nav-tab-active' );
 
-		$( ".tabs-content form" ).children(".tab-content").addClass( "hidden" );
-		$( ".tabs-content form div" + tstatsHash.replace( "#", "#tab-" ) ).removeClass( "hidden" );
+		$( '.tabs-content form' ).children( '.tab-content' ).addClass( 'hidden' );
+		$( '.tabs-content form div' + tstatsHash.replace( '#', '#tab-' ) ).removeClass( 'hidden' );
 	}
 
-	$( ".nav-tab-wrapper a" ).click( function() {
-		var tstatsTabID = $( this ).attr( "href" ).replace( "#", "#tab-" );
+	$( '.nav-tab-wrapper a' ).click( function() {
+		var tstatsTabID = $( this ).attr( 'href' ).replace( '#', '#tab-' );
 
-		// active tab
-		$( this ).parent().children().removeClass( "nav-tab-active" );
-		$( this ).addClass( "nav-tab-active" );
+		// Active tab.
+		$( this ).parent().children().removeClass( 'nav-tab-active' );
+		$( this ).addClass( 'nav-tab-activ e' );
 
-		// active tab content
-		$( ".tabs-content form" ).children( ".tab-content" ).addClass( "hidden" );
-		$( ".tabs-content form div" + tstatsTabID ).removeClass( "hidden" );
+		// Active tab content.
+		$( '.tabs-content form' ).children( '.tab-content' ).addClass( 'hidden' );
+		$( '.tabs-content form div' + tstatsTabID ).removeClass( 'hidden' );
+
 	});
 
 });
