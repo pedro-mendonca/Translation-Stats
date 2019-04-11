18a0feb4e6a7344d408beb5c906f212cb1265802
pedro-mendonca <ped.gaspar@gmail.com>
2019-04-11 16:52:24 +0100

    Add link customizer

--- includes/classes/class-tstats-globals.php
+++ includes/classes/class-tstats-globals.php
@@ -36,6 +36,29 @@ if ( ! class_exists( 'TStats_Globals' ) ) {
 			return $tstats_language;
 		}
 
+
+		/**
+		 * Add campaign information to URL.
+		 *
+		 * @since 0.9.0
+		 *
+		 * @param string $link      Link to costumize.
+		 * @param string $source    Set utm_source, default is site URL.
+		 * @param string $medium    Set utm_medium, default is 'link'.
+		 * @param string $campaign  Set utm_campaign, default is 'tstats_plugin'.'
+		 *
+		 * @return $campaign_link  Return link with campaign parameteres.
+		 */
+		public function tstats_link( $link, $source, $medium, $campaign ) {
+
+			$utm_source    = ! empty( $source ) ? $source : urlencode( home_url() );
+			$utm_medium    = ! empty( $medium ) ? $medium : 'link';
+			$utm_campaign  = ! empty( $campaign ) ? $campaign : 'tstats_plugin';
+
+			$campaign_link = $link . '?utm_source=' . $utm_source . '&amp;utm_medium=' . $medium . '&amp;utm_campaign=' . $campaign;
+			return $campaign_link;
+		}
+
 	}
 
 }
