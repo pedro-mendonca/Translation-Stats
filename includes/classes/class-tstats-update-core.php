841bf6c1758eb420be4087da75307f011407ab3a
pedro-mendonca <ped.gaspar@gmail.com>
2019-12-09 12:16:33 +0000

    Moved generic code to specific class and add admin notice to dashboard.

--- includes/classes/class-tstats-update-core.php
+++ includes/classes/class-tstats-update-core.php
@@ -7,8 +7,6 @@
  * @since 0.9.5
  */
 
-use Gettext\Translations;
-
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -36,17 +34,17 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 			// Instantiate Translation Stats Translations API.
 			$this->tstats_translations_api = new TStats_Translations_API();
 
-			// Instantiate Translation Stats Gettext.
-			$this->tstats_gettext = new TStats_Gettext();
+			// Instantiate Translation Stats Update Translations.
+			$this->tstats_update_translations = new TStats_Update_Translations();
 
-			// Add Translation Stats WordPress info.
-			add_action( 'core_upgrade_preamble', array( $this, 'tstats_update_wordpress_translation_info' ) );
+			// Add WordPress translation info and update button to updates page.
+			add_action( 'core_upgrade_preamble', array( $this, 'tstats_updates_wp_translation_notice' ) );
 
 			// Load WordPress translation updater.
 			add_action( 'wp_ajax_tstats_update_core_content_load', array( $this, 'tstats_update_core_content_load' ) );
 
-			// Add notice to Dashboard.
-			// add_action( 'admin_notices', array( $this, 'tstats_dashboard_wordpress_translation_info' ) ); // phpcs:ignore - Recheck if needed.
+			// Add WordPress translation info admin notice to Dashboard.
+			add_action( 'admin_notices', array( $this, 'tstats_dashboard_wp_translation_notice' ) );
 
 			// Filter 'update_core' transient to prevent update of previous WordPress version language pack.
 			add_filter( 'pre_set_site_transient_update_core', array( $this, 'tstats_remove_previous_wp_translation' ) );
@@ -57,9 +55,9 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 		/**
 		 * Show WordPress core translation info message on Dashboard.
 		 *
-		 * @since 0.9.5
+		 * @since 0.9.5.2
 		 */
-		public function tstats_dashboard_wordpress_translation_info() {
+		public function tstats_dashboard_wp_translation_notice() {
 
 			// Get current screen.
 			$current_screen = get_current_screen();
@@ -73,11 +71,66 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 				return;
 			}
 
-			$notice_args = array(
+			// Get Translation Stats Locale data.
+			$tstats_language = $this->tstats_globals->tstats_translation_language();
+			$locale          = $this->tstats_translations_api->tstats_locale( $tstats_language );
+
+			// Get WordPress core version info.
+			$wp_version = $this->tstats_translations_api->tstats_wordpress_version();
+
+			// Get available translations transient data.
+			$available_translations = get_site_transient( 'available_translations' );
+			if ( empty( $available_translations ) ) {
+				require_once ABSPATH . 'wp-admin/includes/translation-install.php';
+				wp_get_available_translations();
+				$available_translations = get_site_transient( 'available_translations' );
+			}
+
+			// Check if the current translation version is different the WordPress installed version.
+			if ( substr( $available_translations[ $locale['wp_locale'] ]['version'], 0, 3 ) === substr( $wp_version['number'], 0, 3 ) ) {
+				return;
+			}
+
+			$admin_notice_message_status = sprintf(
+				'%s</br>%s',
+				sprintf(
+					wp_kses_post(
+						/* translators: 1: WordPress version. 2: Locale name. */
+						__( 'The translation of WordPress %1$s for %2$s is not complete.', 'translation-stats' )
+					),
+					'<strong>' . esc_html( $wp_version['name'] ) . '</strong>',
+					'<strong>' . esc_html( $locale['native_name'] ) . '</strong>'
+				),
+				sprintf(
+					wp_kses_post(
+						/* translators: 1: Opening link tag <a href="[link]">. 2: Closing link tag </a>. 3: Opening link tag <a href="[link]">. 4: Locale name. */
+						__( 'Please register at %1$sTranslating WordPress%2$s and join the %3$sTranslation Team%2$s to help translating WordPress to %4$s!', 'translation-stats' )
+					),
+					'<a href="https://translate.wordpress.org/locale/' . esc_html( $locale['slug']['locale'] ) . '/' . $locale['slug']['variant'] . '/wp/' . esc_html( $wp_version['slug'] ) . '/" target="_blank">',
+					'</a>',
+					'<a href="https://make.wordpress.org/polyglots/teams/?locale=' . esc_attr( $locale['wp_locale'] ) . '" target="_blank">',
+					'<strong>' . esc_html( $locale['native_name'] ) . '</strong>'
+				)
+			);
+			$admin_notice_message_forceupdate = sprintf(
+				/* translators: %s: Button label. */
+				esc_html__( 'Click the %s button to force update the latest approved translations.', 'translation-stats' ),
+				'<strong>&#8220;' . __( 'Update WordPress Translation', 'translation-stats' ) . '&#8221;</strong>'
+			);
+
+			$admin_notice = array(
+				'type'        => 'warning',
 				'inline'      => false,
 				'dismissible' => true,
+				'force_show'  => true,
+				'message'     => sprintf(
+					'%s</p><p>%s',
+					$admin_notice_message_status,
+					$admin_notice_message_forceupdate
+				),
+				'extra-html'  => $this->tstats_form_update_wordpress_translation(),
 			);
-			$this->tstats_wordpress_translation_info_message( $notice_args );
+			$this->tstats_notices->tstats_notice_message( $admin_notice );
 
 		}
 
@@ -85,9 +138,9 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 		/**
 		 * Add WordPress core info and update button on the Updates page bottom.
 		 *
-		 * @since 0.9.5
+		 * @since 0.9.5.2
 		 */
-		public function tstats_update_wordpress_translation_info() {
+		public function tstats_updates_wp_translation_notice() {
 
 			// End if Translation Stats language is 'en_US'.
 			if ( $this->tstats_globals->tstats_language_is_english() ) {
@@ -113,21 +166,12 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 			<div class="translation-stats-update-core-info">
 
 				<?php
-				// Show force update WordPress translation button.
-				$form_action = 'update-core.php?tstats=force_update_core';
-				?>
+				// Add form with action button to update WordPress core translation.
+				echo wp_kses( $this->tstats_form_update_wordpress_translation(), $this->tstats_globals->tstats_allowed_html() );
 
-				<form method="post" action="<?php echo esc_url( $form_action ); ?>" name="upgrade-wordpress-translation" class="upgrade">
-					<?php wp_nonce_field( 'upgrade-wordpress-translation' ); ?>
-					<p>
-						<input type="submit" name="force_update_core" class="button button-primary" value="<?php esc_attr_e( 'Update WordPress Translation', 'translation-stats' ); ?>">
-					</p>
-				</form>
-
-				<?php
 				// Show the Translation Stats admin notice for WordPress core translation status.
 				$notice_args = array();
-				$this->tstats_wordpress_translation_info_message( $notice_args );
+				$this->tstats_updates_wp_translation_notice_message( $notice_args );
 				?>
 
 			</div>
@@ -151,13 +195,37 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 
 
 		/**
-		 * WordPress core translation info message.
+		 * Add form with action button to update WordPress core translation.
 		 *
-		 * @since 0.9.5
+		 * @since 0.9.5.2
+		 */
+		public function tstats_form_update_wordpress_translation() {
+
+			// Show force update WordPress translation button.
+			$form_action = 'update-core.php?tstats=force_update_core';
+			ob_start();
+			?>
+
+			<form method="post" action="<?php echo esc_url( $form_action ); ?>" name="upgrade-wordpress-translation" class="upgrade">
+				<?php wp_nonce_field( 'upgrade-wordpress-translation' ); ?>
+				<p>
+					<input type="submit" name="force_update_core" class="button button-primary" value="<?php esc_attr_e( 'Update WordPress Translation', 'translation-stats' ); ?>">
+				</p>
+			</form>
+
+			<?php
+			return ob_get_clean();
+		}
+
+
+		/**
+		 * WordPress updates translation info message.
+		 *
+		 * @since 0.9.5.2
 		 *
 		 * @param array $notice_args  Arguments for admin notice.
 		 */
-		public function tstats_wordpress_translation_info_message( $notice_args ) {
+		public function tstats_updates_wp_translation_notice_message( $notice_args ) {
 
 			// Get Translation Stats Locale data.
 			$tstats_language = $this->tstats_globals->tstats_translation_language();
@@ -298,7 +366,7 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 				</h4>
 
 				<?php
-				$result = $this->tstats_update_translation( $destination, $project, $tstats_language );
+				$result = $this->tstats_update_translations->tstats_update_translation( $destination, $project, $tstats_language );
 				if ( is_wp_error( $result['data'] ) ) {
 					$error_message = $result['data']->get_error_message();
 					$admin_notice  = array(
@@ -364,235 +432,6 @@ if ( ! class_exists( 'TStats_Update_Core' ) ) {
 
 			<?php
 			wp_die();
-
-		}
-
-
-		/**
-		 * Update project translation.
-		 * Download .po file, extract with Gettext to generate .po and .json files.
-		 *
-		 * @param string $destination   Local destination of the language file. ( e.g: local/site/wp-content/languages/ ).
-		 * @param array  $project       Project array.
-		 * @param string $wp_locale     WP Locale ( e.g.: 'pt_PT' ).
-		 * @return array|WP_Error       Array on success, WP_Error on failure.
-		 */
-		public function tstats_update_translation( $destination, $project, $wp_locale ) {
-
-			// Set array of log entries.
-			$result['log'] = array();
-
-			// Get Translation Stats Locale data.
-			$locale = $this->tstats_translations_api->tstats_locale( $wp_locale );
-
-			// Download file from WordPress.org translation table.
-			$download = $this->tstats_download_translations( $project, $locale );
-			array_push( $result['log'], $download['log'] );
-			$result['data'] = $download['data'];
-			if ( is_wp_error( $result['data'] ) ) {
-				return $result;
-			}
-
-			// Generate .po from WordPress.org response.
-			$generate_po = $this->tstats_generate_po( $destination, $project, $locale, $download['data'] );
-			array_push( $result['log'], $generate_po['log'] );
-			$result['data'] = $generate_po['data'];
-			if ( is_wp_error( $result['data'] ) ) {
-				return $result;
-			}
-
-			// Extract translations from file.
-			$translations   = $this->tstats_extract_translations( $destination, $project, $locale );
-			$result['data'] = $translations['data'];
-			if ( is_wp_error( $result['data'] ) ) {
-				return $result;
-			}
-
-			// Generate .mo file from extracted translations.
-			$generate_mo = $this->tstats_generate_mo( $destination, $project, $locale, $translations['data'] );
-			array_push( $result['log'], $generate_mo['log'] );
-			$result['data'] = $generate_mo['data'];
-			if ( is_wp_error( $result['data'] ) ) {
-				return $result;
-			}
-
-			// Generate .json files from extracted translations.
-			$generate_jsons = $this->tstats_gettext->tstats_make_json( $destination, $project, $locale, $translations['data'], false );
-			$result['log']  = array_merge( $result['log'], $generate_jsons['log'] );
-			$result['data'] = $generate_jsons['data'];
-			if ( is_wp_error( $result['data'] ) ) {
-				return $result;
-			}
-
-			return $result;
-
-		}
-
-
-		/**
-		 * Download file from WordPress.org translation table.
-		 *
-		 * @param array $project    Project array.
-		 * @param array $locale     Locale array.
-		 * @return array|WP_Error   Array on success, WP_Error on failure.
-		 */
-		public function tstats_download_translations( $project, $locale ) {
-
-			// Get WordPress core version info.
-			$wp_version = $this->tstats_translations_api->tstats_wordpress_version();
-
-			// Set translation data path.
-			$source = $this->tstats_translations_api->tstats_translation_path( $wp_version, $project, $locale );
-
-			// Report message.
-			$result['log'] = sprintf(
-				/* translators: %s: URL. */
-				esc_html__( 'Downloading translation from %s…', 'translation-stats' ),
-				'<code>' . esc_html( $source ) . '</code>'
-			);
-
-			// Get the translation data.
-			$response = wp_remote_get( $source );
-
-			if ( ! is_array( $response ) || 'application/octet-stream' !== $response['headers']['content-type'] ) {
-
-				// Report message.
-				$result['data'] = new WP_Error(
-					'download-translation',
-					sprintf(
-						'%s %s',
-						esc_html__( 'Download failed.', 'translation-stats' ),
-						esc_html__( 'A valid URL was not provided.', 'translation-stats' )
-					)
-				);
-				return $result;
-
-			}
-
-			$result['data'] = $response;
-
-			return $result;
-
-		}
-
-
-		/**
-		 * Generate .po from WordPress.org response.
-		 *
-		 * @param string $destination   Local destination of the language file. ( e.g: local/site/wp-content/languages/ ).
-		 * @param array  $project       Project array.
-		 * @param array  $locale        Locale array.
-		 * @param array  $response      HTTP response.
-		 * @return array|WP_Error       Array on success, WP_Error on failure.
-		 */
-		public function tstats_generate_po( $destination, $project, $locale, $response ) {
-
-			// Set the file naming convention. ( e.g.: {domain}-{locale}.po ).
-			$domain    = $project['domain'] ? $project['domain'] . '-' : '';
-			$file_name = $domain . $locale['wp_locale'] . '.po';
-
-			// Report message.
-			$result['log'] = sprintf(
-				/* translators: %s: File name. */
-				esc_html__( 'Saving file %s…', 'translation-stats' ),
-				'<code>' . esc_html( $file_name ) . '</code>'
-			);
-
-			// Generate .po file.
-			$success = file_put_contents( $destination . $file_name, $response['body'] ); // phpcs:ignore
-
-			if ( ! $success ) {
-
-				// Report message.
-				$result['data'] = new WP_Error(
-					'generate-po',
-					esc_html__( 'Could not create file.', 'translation-stats' )
-				);
-				return $result;
-
-			}
-
-			$result['data'] = true;
-
-			return $result;
-
-		}
-
-
-		/**
-		 * Extract translations from file.
-		 *
-		 * @param string $destination   Local destination of the language file. ( e.g: local/site/wp-content/languages/ ).
-		 * @param array  $project       Project array.
-		 * @param array  $locale        Locale array.
-		 * @return array|WP_Error       Array on success, WP_Error on failure.
-		 */
-		public function tstats_extract_translations( $destination, $project, $locale ) {
-
-			// Set the file naming convention. ( e.g.: {domain}-{locale}.po ).
-			$domain    = $project['domain'] ? $project['domain'] . '-' : '';
-			$file_name = $domain . $locale['wp_locale'] . '.po';
-
-			$translations = Gettext\Translations::fromPoFile( $destination . $file_name );
-
-			if ( ! $translations ) {
-
-				// Report message.
-				$result['data'] = new WP_Error(
-					'extract-translations',
-					esc_html__( 'Could not extract file.', 'translation-stats' )
-				);
-
-				return $result;
-
-			}
-
-			$result['data'] = $translations;
-
-			return $result;
-
-		}
-
-
-		/**
-		 * Generate .mo file from extracted translations.
-		 *
-		 * @param string $destination    Local destination of the language file. ( e.g: local/site/wp-content/languages/ ).
-		 * @param array  $project        Project array.
-		 * @param array  $locale         Locale array.
-		 * @param object $translations   Extracted translations to export.
-		 * @return array|WP_Error        Array on success, WP_Error on failure.
-		 */
-		public function tstats_generate_mo( $destination, $project, $locale, $translations ) {
-
-			// Set the file naming convention. ( e.g.: {domain}-{locale}.po ).
-			$domain    = $project['domain'] ? $project['domain'] . '-' : '';
-			$file_name = $domain . $locale['wp_locale'] . '.mo';
-
-			// Report message.
-			$result['log'] = sprintf(
-				/* translators: %s: File name. */
-				esc_html__( 'Saving file %s…', 'translation-stats' ),
-				'<code>' . $file_name . '</code>'
-			);
-
-			// Generate .mo file.
-			$generate = $translations->toMoFile( $destination . $file_name );
-
-			if ( ! $generate ) {
-
-				// Report message.
-				$result['data'] = new WP_Error(
-					'generate-mo',
-					esc_html__( 'Could not create file.', 'translation-stats' )
-				);
-				return $result;
-
-			}
-
-			$result['data'] = true;
-
-			return $result;
 
 		}
 
