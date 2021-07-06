50c3198e1582cf42bf9d9e4cebdce21f173b120d
pedro-mendonca <ped.gaspar@gmail.com>
2021-07-06 14:11:08 +0100

    Test up to WP 5.8

--- translation-stats.php
+++ translation-stats.php
@@ -15,7 +15,7 @@
  * Description:       Show plugins translation stats on your WordPress install.
  * Version:           1.1.2
  * Requires at least: 4.9
- * Tested up to:      5.7
+ * Tested up to:      5.8
  * Requires PHP:      5.6
  * Author:            Pedro Mendonça
  * Author URI:        https://translationstats.com
