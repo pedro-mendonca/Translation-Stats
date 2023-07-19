884d200b5e19f3eaea39244d7b99fec9be64724d
pedro-mendonca <ped.gaspar@gmail.com>
2023-07-19 17:49:34 +0100

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,21 +32,32 @@ module.exports = function( grunt ) {
 						return readme;
 					},
 					post_convert: function( readme ) {
-						var header = "# Translation Stats\n\nShow plugins translation stats on your WordPress install.\n\n[![Translation Stats banner](./.wordpress-org/banner-1544x500.png)](https://wordpress.org/plugins/translation-stats/)\n\n[![WordPress Plugin Version](https://img.shields.io/wordpress/plugin/v/translation-stats?label=Plugin%20Version&logo=wordpress)](https://wordpress.org/plugins/translation-stats/)\n[![WordPress Plugin Rating](https://img.shields.io/wordpress/plugin/stars/translation-stats?label=Plugin%20Rating&logo=wordpress)](https://wordpress.org/support/plugin/translation-stats/reviews/)\n[![WordPress Plugin Downloads](https://img.shields.io/wordpress/plugin/dt/translation-stats.svg?label=Downloads&logo=wordpress)](https://wordpress.org/plugins/translation-stats/advanced/)\n[![Sponsor](https://img.shields.io/badge/GitHub-🤍%20Sponsor-ea4aaa?logo=github)](https://github.com/sponsors/pedro-mendonca)\n\n[![WordPress Plugin Required PHP Version](https://img.shields.io/wordpress/plugin/required-php/translation-stats?label=PHP%20Required&logo=php&logoColor=white)](https://wordpress.org/plugins/translation-stats/)\n[![WordPress Plugin: Required WP Version](https://img.shields.io/wordpress/plugin/wp-version/translation-stats?label=WordPress%20Required&logo=wordpress)](https://wordpress.org/plugins/translation-stats/)\n[![WordPress Plugin: Tested WP Version](https://img.shields.io/wordpress/plugin/tested/translation-stats.svg?label=WordPress%20Tested&logo=wordpress)](https://wordpress.org/plugins/translation-stats/)\n\n[![Coding Standards](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/coding-standards.yml/badge.svg)](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/coding-standards.yml)\n[![Static Analysis](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/static-analysis.yml/badge.svg)](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/static-analysis.yml)\n[![Codacy Badge](https://api.codacy.com/project/badge/Grade/bcd1b44a1d6542e2b75b7b479ce56804)](https://www.codacy.com/app/pedro-mendonca/Translation-Stats?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=pedro-mendonca/Translation-Stats&amp;utm_campaign=Badge_Grade)\n\n";
 
-							// Usar o título e descrição abaixo.
-
-						// Remove title.
-						readme = readme.replace( new RegExp( "(^\# Translation Stats \#\\n\\nShow plugins translation stats on your WordPress install.\n\n)", "gim" ), "" );
+						// Custom header.
+						var header = "[![Translation Stats banner](./.wordpress-org/banner-1544x500.png)](https://wordpress.org/plugins/translation-stats/)\n";
+						header += "\n";
+						header += "[![WordPress Plugin Version](https://img.shields.io/wordpress/plugin/v/translation-stats?label=Plugin%20Version&logo=wordpress)](https://wordpress.org/plugins/translation-stats/)";
+						header += "[![WordPress Plugin Rating](https://img.shields.io/wordpress/plugin/stars/translation-stats?label=Plugin%20Rating&logo=wordpress)](https://wordpress.org/support/plugin/translation-stats/reviews/)";
+						header += "[![WordPress Plugin Downloads](https://img.shields.io/wordpress/plugin/dt/translation-stats.svg?label=Downloads&logo=wordpress)](https://wordpress.org/plugins/translation-stats/advanced/)";
+						header += "[![Sponsor](https://img.shields.io/badge/GitHub-🤍%20Sponsor-ea4aaa?logo=github)](https://github.com/sponsors/pedro-mendonca)";
+						header += "\n";
+						header += "[![WordPress Plugin Required PHP Version](https://img.shields.io/wordpress/plugin/required-php/translation-stats?label=PHP%20Required&logo=php&logoColor=white)](https://wordpress.org/plugins/translation-stats/)";
+						header += "[![WordPress Plugin: Required WP Version](https://img.shields.io/wordpress/plugin/wp-version/translation-stats?label=WordPress%20Required&logo=wordpress)](https://wordpress.org/plugins/translation-stats/)";
+						header += "[![WordPress Plugin: Tested WP Version](https://img.shields.io/wordpress/plugin/tested/translation-stats.svg?label=WordPress%20Tested&logo=wordpress)](https://wordpress.org/plugins/translation-stats/)";
+						header += "\n";
+						header += "[![Coding Standards](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/coding-standards.yml/badge.svg)](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/coding-standards.yml)";
+						header += "[![Static Analysis](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/static-analysis.yml/badge.svg)](https://github.com/pedro-mendonca/Translation-Stats/actions/workflows/static-analysis.yml)";
+						header += "[![Codacy Badge](https://api.codacy.com/project/badge/Grade/bcd1b44a1d6542e2b75b7b479ce56804)](https://www.codacy.com/app/pedro-mendonca/Translation-Stats?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=pedro-mendonca/Translation-Stats&amp;utm_campaign=Badge_Grade)";
+						header += "\n";
 
 						// Remove traditional WordPress plugin headers.
-						readme = readme.replace( new RegExp( "(^\\*\\*.+:\\*\\*.+\\s*$\\n)", "gim" ), "" );
+						readme = readme.replace( new RegExp( "(^\\*\\*.+:\\*\\*.+\\s*$\\n)", "gim" ), "newheader" );
+
+						readme = readme.replace( new RegExp( "(newheader)", "gim" ), header );
 
 						console.log( readme );
 
-						readme += '####';
-
-						return header + readme;
+						return readme;
 					}
 				}
 			},
