16c019ba90db77f7e2aed0c1ed84da7c92415cb6
pedro-mendonca <ped.gaspar@gmail.com>
2024-07-04 16:09:08 +0100

    Test translation_stats_check_version()

--- tests/phpunit/test-translation-stats.php
+++ tests/phpunit/test-translation-stats.php
@@ -12,6 +12,13 @@
 class Test_Plugin_File extends WP_UnitTestCase {
 
 	/**
+	 * Test backup sanity check, in case the plugin is activated, or the versions change after activation.
+	 */
+	function test_translation_stats_check_version() {
+		$this->assertNull( Translation_Stats\translation_stats_check_version() );
+	}
+
+	/**
 	 * Test Translation Stats minimum requirements.
  	 */
  	function test_translation_stats_compatible_version() {
