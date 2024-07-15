3433cc2916b55dbdf94a93817d2988d965020b73
pedro-mendonca <ped.gaspar@gmail.com>
2024-07-15 16:55:12 +0100

    Remove test with assertion not supported in old PHPUnit

--- tests/phpunit/includes/test-admin-notice.php
+++ tests/phpunit/includes/test-admin-notice.php
@@ -14,22 +14,6 @@ Use Translation_Stats\Admin_Notice;
 class Test_Admin_Notice extends WP_UnitTestCase {
 
 	/**
-	 * Test display formatted admin notice.
-	 */
-	public function test_message() {
-
-		$admin_notice = array(
-			'type'       => 'error',
-			'notice-alt' => true,
-			'message'    => 'Translation project not found on WordPress.org',
-		);
-		$admin_notice = new Admin_Notice( $admin_notice );
-
-		$this->assertIsObject( $admin_notice );
-
-	}
-
-	/**
 	 * Data provider.
 	 *
 	 * @var array
